63a32a5c0830f181376d2dd3aa4974cf586050e2
GoBrazill <vinicius.daraujos26@gmail.com>
2026-03-10 21:22:06 -0300

    feat: add example on how to declare other pages on react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import ComingSoonPage from './pages/CommingSoonPage.jsx'
 import NotFoundPage from './pages/NotFoundPage.jsx'
-import GlobalStyle from './styles/GlobalStyle.jsx'
+import GlobalStyle from './styles/GlobalStyle.js'
+import ExamplePage from './pages/ExamplePage.jsx'
 
 // Criando uma array para definir as rotas do site utilizando a função createBrowserRouter importada
 const router = createBrowserRouter([
   {
     path: '/',
     element: <ComingSoonPage />,
+  },
+  {
+    path: '/example',
+    element: <ExamplePage />
+  },
+  {
     // Error element aparece no caso de digitar um endereço que não existe, link com caminho errado ou erros de carregamento
     errorElement: <NotFoundPage />
   }
